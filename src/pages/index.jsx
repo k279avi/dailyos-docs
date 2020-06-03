516a7c5119181aad1347ecf0319bd5e93cddbba7
Rishi Singhal <49945376+welcometodailykit@users.noreply.github.com>
2020-06-03 15:10:21 +0530

    Added: Index.js

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -10,21 +10,12 @@ const IndexPage = () => (
       <Header hideMenu={true} />
       <Row>
          <Left>
-            <h2>DailyGit Docs</h2>
+            <h2>DailyOS Docs and Learning Center</h2>
             <p>
-               A headless CMS powered by GraphQL built with Filesystem and Git
-               to provide content versioning. Build your own CMS by defining
-               contextual entity schemas.✨
+               DailyOS is an open source platform for restaurants, grocers, and new age food-preneurs to start and operate their food business.✨
             </p>
             <div>
                <Link to="/docs/introduction">See Docs</Link>
-               <a
-                  href="https://github.com/dailykit/dailygit"
-                  target="_blank"
-                  rel="noopener"
-                  rel="norefferer">
-                  View Source
-               </a>
             </div>
          </Left>
          <div>
